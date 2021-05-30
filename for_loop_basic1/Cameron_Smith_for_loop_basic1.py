c22397a4958e34b9f0e3a3e7d4ee7524454610db
Cameron Smith <theauspiciouscammy@gmail.com>
2021-05-30 14:14:32 -0700

    cleanup commit

--- for_loop_basic1/Cameron_Smith_for_loop_basic1.py
+++ for_loop_basic1/Cameron_Smith_for_loop_basic1.py
@@ -1,17 +1,17 @@
-# for x in range(151):
-#     print(x)
+for x in range(151):
+    print(x)
 
-# for x in range(1001):
-#     if x % 5 == 0:
-#         print(x)
+for x in range(1001):
+    if x % 5 == 0:
+        print(x)
 
-# for x in range(101):
-#     if x % 10 ==0:
-#         print("Coding Dojo")
-#     elif x % 5 ==0:
-#         print("Coding")
-#     elif x<101:
-#         print(x)
+for x in range(101):
+    if x % 10 ==0:
+        print("Coding Dojo")
+    elif x % 5 ==0:
+        print("Coding")
+    elif x<101:
+        print(x)
 
 sum = 0
 for x in range(500001):
@@ -19,12 +19,12 @@ for x in range(500001):
         sum = sum + x
 print(sum)
 
-# for x in range(2018,0,-4):
-#     print(x)
+for x in range(2018,0,-4):
+    print(x)
 
-# low = 2
-# high = 23
-# mult = 4
-# for x in range (low, high+1, 1):
-#     if x % mult ==0:
-#         print(x)
+low = 2
+high = 23
+mult = 4
+for x in range (low, high+1, 1):
+    if x % mult ==0:
+        print(x)
--- functions_int2/Cameron_Smith_funct_int2.py
+++ functions_int2/Cameron_Smith_funct_int2.py
@@ -1,85 +1,85 @@
 #1
 #1A
-# x = [ [5,2,3], [10,8,9] ]
-# def nestlist(input):
-#     input[1][0] = 15
-#     return input
-# print(nestlist(x))
+x = [ [5,2,3], [10,8,9] ]
+def nestlist(input):
+    input[1][0] = 15
+    return input
+print(nestlist(x))
 
 #1B
-# students = [
-#     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#     {'first_name' : 'John', 'last_name' : 'Rosales'}
-# ]
-# def nameSwap(input):
-#     input[0]['last_name'] = 'Bryant'
-#     return input
-# print(nameSwap(students))
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+def nameSwap(input):
+    input[0]['last_name'] = 'Bryant'
+    return input
+print(nameSwap(students))
 
 #1C
-# sports_directory = {
-#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
-# }
-# def dirSwap(input):
-#     input['soccer'][0] = 'Andres'
-#     return input
-# print(dirSwap(sports_directory))
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+def dirSwap(input):
+    input['soccer'][0] = 'Andres'
+    return input
+print(dirSwap(sports_directory))
 
 #1D
-# z = [ {'x': 10, 'y': 20} ]
-# def valSwap(input):
-#     input[0]['y'] = 30
-#     return input
-# print(valSwap(z))
+z = [ {'x': 10, 'y': 20} ]
+def valSwap(input):
+    input[0]['y'] = 30
+    return input
+print(valSwap(z))
 
-# #2
-# def iterateDictionary(mylist):
-#     for i in range(len(mylist)):
-#         print("first_name -", mylist[i]['first_name'],"last_name -", mylist[i]['last_name'])
-# students = [{'first_name':  'Michael,', 'last_name' : 'Jordan'},
-#             {'first_name' : 'John,', 'last_name' : 'Rosales'},
-#             {'first_name' : 'Mark,', 'last_name' : 'Guillen'},
-#             {'first_name' : 'KB,', 'last_name' : 'Tonel'}]
-# iterateDictionary(students)
+#2
+def iterateDictionary(mylist):
+    for i in range(len(mylist)):
+        print("first_name -", mylist[i]['first_name'],"last_name -", mylist[i]['last_name'])
+students = [{'first_name':  'Michael,', 'last_name' : 'Jordan'},
+            {'first_name' : 'John,', 'last_name' : 'Rosales'},
+            {'first_name' : 'Mark,', 'last_name' : 'Guillen'},
+            {'first_name' : 'KB,', 'last_name' : 'Tonel'}]
+iterateDictionary(students)
 
-# #3
-# students = [
-#     {'first_name': 'Michael', 'last_name': 'Jordan'},
-#     {'first_name': 'John', 'last_name': 'Rosales'},
-#     {'first_name': 'Mark', 'last_name': 'Guillen'},
-#     {'first_name': 'KB', 'last_name': 'Tonel'}
-# ]
-# def iterateDictionary2(a, list):
-#     for i in range(len(list)):
-#         print(list[i][a])
-# iterateDictionary2('first_name', students)
-# iterateDictionary2('last_name', students)
+#3
+students = [
+    {'first_name': 'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+def iterateDictionary2(a, list):
+    for i in range(len(list)):
+        print(list[i][a])
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
 
 #4
-# dojo = {
-#     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-#     'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-# }
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
 #4.0
-# def printInfo(lib):
-#     for i in range(len(lib)-1):
-#         numL = len(lib['locations'])
-#         print(numL, 'LOCATIONS')
-#     for i in range(len(lib['locations'])):
-#         print(lib['locations'][i])
-#     for i in range(len(lib)-1):
-#         numI = len(lib['instructors'])
-#         print(numI, 'INSTRUCTORS')
-#     for i in range(len(lib['instructors'])):
-#         print(lib['instructors'][i])
-# printInfo(dojo)
+def printInfo(lib):
+    for i in range(len(lib)-1):
+        numL = len(lib['locations'])
+        print(numL, 'LOCATIONS')
+    for i in range(len(lib['locations'])):
+        print(lib['locations'][i])
+    for i in range(len(lib)-1):
+        numI = len(lib['instructors'])
+        print(numI, 'INSTRUCTORS')
+    for i in range(len(lib['instructors'])):
+        print(lib['instructors'][i])
+printInfo(dojo)
 
 
 #4.1
-# def printInfo(lib):
-#     for i in lib:
-#         print(len(lib[i]), i.upper())
-#         for list in lib[i]:
-#             print(list)
-# printInfo(dojo)
+def printInfo(lib):
+    for i in lib:
+        print(len(lib[i]), i.upper())
+        for list in lib[i]:
+            print(list)
+printInfo(dojo)
